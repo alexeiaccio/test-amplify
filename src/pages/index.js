42e2d83dd2de64ae9e496dd10144a9c40e02e310
Alexei Accio <alexei.accio@gmail.com>
2018-12-10 15:46:44 +0300

    mutation graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { graphql } from 'gatsby'
 import { API, graphqlOperation } from 'aws-amplify'
+import uuid from 'uuid/v4'
+
 import { setConfig } from 'react-hot-loader'
 
 import { listPosts } from 'graphql/queries'
+import { createPost } from 'graphql/mutations'
 
 setConfig({ pureSFC: true });
 
 function IndexPage({ data }) {
+  const inputEl = useRef(null)
   const { listPosts: { items } } = data.posts
   const [posts, setPosts] = useState([])
+  const [title, setTitle] = useState('')
 
   useEffect(() => {
     (async () => {
@@ -19,10 +24,32 @@ function IndexPage({ data }) {
     })()
     return setPosts([])
   }, [])
+
+  const handleInput = () => {
+    setTitle(inputEl.current.value)
+  }
+
+  const addPost = async (e) => {
+    e.preventDefault()
+    setTitle('')
+    const createPostInput = {
+      input: {
+        id: uuid(),
+        title: title,
+        date: new Date().valueOf(),
+        description: title
+      }
+    }
+    const { data, errors } = await API.graphql(graphqlOperation(createPost, createPostInput))
+    if (data) {
+      setPosts(posts.concat(data))
+    }
+    if (errors) console.error(errors)
+  }
   
   return (
-    <div>
-      Poop!
+    <>
+      <h1>Poop!</h1>
       <ul>
         {items.map(({id, title}) => (
           <li key={id}>{title}</li>
@@ -33,7 +60,11 @@ function IndexPage({ data }) {
           <li key={id}>{title}</li>
         ))}
       </ul>
-    </div>
+      <form onSubmit={addPost}>
+        <input ref={inputEl} onChange={handleInput} type="text" />
+        <button type="submit">Add new post</button>
+      </form>
+    </>
   )
 }
 
